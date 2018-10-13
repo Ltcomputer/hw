2b20460be1043cfeaf89f47b2c6c50e91bff373e
Ltcomputer <1562510567@qq.com>
2018-10-13 11:50:37 +0800

    购物车功能

--- assets/index.js
+++ assets/index.js
@@ -15,20 +15,27 @@ $('.tab .menus li').each(function(){
         // $('.tab .scroll').css('margin-top',-index*800+'px');
         $('.tab .scroll').css('margin-top',-index*484+'px');
         $('.right').css('display','block');
-        $('.tab .menus li').css('width','600px');
+        $('.tab .menus li').css('width','1200px');
         // $('.tab').css('background-image','url('+arr[index]+')')// 背景图片  不需要注释即可
-        $('.tab .category-box').mouseout(function(){
-
-            // $('.tab .menus li').removeClass('bg');
-            // $(this).addClass('bg');
-            // var index = $(this).index();
-          
-            
-            // $('.right').css('display','none');
-            // $('.tab').css('background-image','url('+arr[index]+')')// 背景图片  不需要注释即可
-        })
-    
+        
     })
+    
+    
+    
+    
+    $('.category-name').on('mouseleave',function(){
+
+        // $('.tab .menus li').removeClass('bg');
+        // $(this).addClass('bg');
+        // var index = $(this).index();
+      
+        // $('.tab .menus li').css('width','200px');
+        $('.right').css('display','none');
+       
+    })
+
+
+
 },
 
 )
